ae824523595071fcb0d6d71ed952ec8b364eacb5
Abdullah Tharwat <visualsbyabd257@gmail.com>
2025-04-16 07:50:02 +0200

    Global Components Package Init

--- components/globals/index.ts
+++ components/globals/index.ts
@@ -1,2 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+export { default as GlowCard } from "./GlowCard";
+export { default as TitleHeader } from "./TitleHeader";
