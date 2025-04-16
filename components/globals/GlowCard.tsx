724ad24c9ce6b09a27da6083b84bf00d67b8c7b5
Abdullah Tharwat <visualsbyabd257@gmail.com>
2025-04-16 07:51:55 +0200

    Glow Card

--- components/globals/GlowCard.tsx
+++ components/globals/GlowCard.tsx
@@ -1,52 +1,48 @@
-import { useRef, MouseEvent } from "react";
+"use client";
+import { useRef } from "react";
 
-interface CardData {
-  title: string | null;
-}
-
-interface GlowCardProps {
-  card: CardData;
+const GlowCard: React.FC<{
   index: number;
   children: React.ReactNode;
-}
-
-const GlowCard: React.FC<GlowCardProps> = ({ card, index, children }) => {
+  className?: string;
+}> = ({ index, children, className }) => {
   // refs for all the cards
   const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   // when mouse moves over a card, rotate the glow effect
-  const handleMouseMove =
-    (index: number) => (e: MouseEvent<HTMLDivElement>) => {
-      const card = cardRefs.current[index];
-      if (!card) return;
+  const handleMouseMove = (index: number) => (e: React.MouseEvent) => {
+    // get the current card
+    const card = cardRefs.current[index];
+    if (!card) return;
 
-      const rect = card.getBoundingClientRect();
-      const mouseX = e.clientX - rect.left - rect.width / 2;
-      const mouseY = e.clientY - rect.top - rect.height / 2;
+    // get the mouse position relative to the card
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
 
-      let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
-      angle = (angle + 360) % 360;
+    // calculate the angle from the center of the card to the mouse
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
 
-      card.style.setProperty("--start", `${angle + 60}`);
-    };
+    // adjust the angle so that it's between 0 and 360
+    angle = (angle + 360) % 360;
 
+    // set the angle as a CSS variable
+    card.style.setProperty("--start", (angle + 60).toString());
+  };
+
+  // return the card component with the mouse move event
   return (
     <div
       ref={(el) => {
         cardRefs.current[index] = el;
       }}
       onMouseMove={handleMouseMove(index)}
-      className="card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column"
+      className={
+        "card card-border timeline-card rounded-xl break-inside-avoid-column " +
+        className
+      }
     >
       <div className="glow"></div>
-      <div className="flex items-center gap-1 mb-5">
-        {Array.from({ length: 5 }, (_, i) => (
-          <img key={i} src="/images/star.png" alt="star" className="size-5" />
-        ))}
-      </div>
-      <div className="mb-5">
-        <p className="text-white-50 text-lg">{card.title ?? ""}</p>
-      </div>
       {children}
     </div>
   );
