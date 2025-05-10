35b81fd471a9d19fa07f1a54418cb19ab1692166
Abdullah Tharwat <visualsbyabd257@gmail.com>
2025-05-10 09:13:47 +0300

    Number Formatter

---
+++ utils/formatters.ts
@@ -0,0 +1,11 @@
+export function formatNumber(num: number): string {
+  if (num < 1000) {
+    return num.toString();
+  } else if (num >= 1000 && num < 1000000) {
+    return Math.floor(num / 1000) + "K";
+  } else if (num >= 1000000 && num < 1000000000) {
+    return Math.floor(num / 1000000) + "M";
+  } else {
+    return Math.floor(num / 1000000000) + "B";
+  }
+}
