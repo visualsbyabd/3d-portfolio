8b42d7b201cb81eaf6101932744ba8fb3b050cd1
Abdullah Tharwat <visualsbyabd257@gmail.com>
2025-05-10 09:14:00 +0300

    Featured Videos Section

---
+++ components/home/VideosFeaturedProjectsSection.tsx
@@ -0,0 +1,198 @@
+"use client";
+import React, { useRef, useState, useEffect, RefObject } from "react";
+import { GlowCard, TitleHeader } from "@/components/globals";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faEye, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { formatNumber } from "@/utils/formatters";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const videos: Video[] = [
+  {
+    title: "Video #1",
+    description:
+      "A high-energy visual capturing movement and excitement in a short format.",
+    platformType: ["INSTAGRAM", "TIKTOK"],
+    thumbnail: "/videos-thumbnails/video-1.jpg",
+    video: "./videos/video-1.mp4",
+    totalViews: 12000,
+    totalLikes: 950,
+    ref: null,
+  },
+  {
+    title: "Video #2",
+    description:
+      "An insightful look into a creative concept that sparks curiosity and engagement.",
+    platformType: ["FACEBOOK", "YOUTUBE"],
+    thumbnail: "/videos-thumbnails/video-2.jpg",
+    video: "./videos/video-2.mp4",
+    totalViews: 56000,
+    totalLikes: 4800,
+    ref: null,
+  },
+  {
+    title: "Video #3",
+    description:
+      "A visually dynamic scene with smooth transitions and engaging storytelling.",
+    platformType: ["INSTAGRAM", "YOUTUBE"],
+    thumbnail: "/videos-thumbnails/video-3.jpg",
+    video: "./videos/video-3.mp4",
+    totalViews: 30500,
+    totalLikes: 2100,
+    ref: null,
+  },
+  {
+    title: "Video #4",
+    description:
+      "A cinematic approach to content, capturing emotions and moments vividly.",
+    platformType: ["TIKTOK", "YOUTUBE"],
+    thumbnail: "/videos-thumbnails/video-4.jpg",
+    video: "./videos/video-4.mp4",
+    totalViews: 47000,
+    totalLikes: 3200,
+    ref: null,
+  },
+  {
+    title: "Video #5",
+    description:
+      "An informative breakdown of an interesting topic, engaging viewers with clarity.",
+    platformType: ["FACEBOOK", "INSTAGRAM"],
+    thumbnail: "/videos-thumbnails/video-5.jpg",
+    video: "./videos/video-5.mp4",
+    totalViews: 25000,
+    totalLikes: 1800,
+    ref: null,
+  },
+  {
+    title: "Video #6",
+    description:
+      "A fresh take on a trending idea, creatively executed for maximum impact.",
+    platformType: ["TIKTOK", "FACEBOOK"],
+    thumbnail: "/videos-thumbnails/video-6.jpg",
+    video: "./videos/video-6.mp4",
+    totalViews: 61000,
+    totalLikes: 5000,
+    ref: null,
+  },
+];
+
+interface Video {
+  title: string;
+  description: string;
+  platformType: string[];
+  thumbnail: string;
+  video: string;
+  totalViews: number;
+  totalLikes: number;
+  ref: RefObject<HTMLDivElement | null> | null;
+}
+
+const VideosFeaturedProjectsSection = () => {
+  const sectionRef = useRef(null);
+
+  useGSAP(() => {
+    // Animation for the main section
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 },
+    );
+  });
+
+  useEffect(() => {
+    // Create a new array of refs whenever items change
+    videos.forEach((video, i) => {
+      videos[i].ref = React.createRef();
+      return;
+    });
+
+    videos.forEach((video, index) => {
+      gsap.fromTo(
+        video.ref?.current,
+        {
+          y: 100,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.5,
+          delay: 0.3 * index + 2,
+          scrollTrigger: {
+            trigger: video.ref?.current,
+            start: "top bottom-=100",
+          },
+        },
+      );
+    });
+  }, [videos]);
+
+  return (
+    <section
+      ref={sectionRef}
+      className="w-full border-2 border-red-500 max-w-7xl min-h-[50vh] flex flex-col items-center gap-4 justify-center px-4 py-8"
+    >
+      <div className="w-full items-center justify-start gap-2 flex flex-col">
+        <TitleHeader title="Featured Videos" sub="🚀 Get Visually Obsessed" />
+      </div>
+      <div className="w-full h-full grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 items-center gap-4">
+        {videos.map((video, i) => (
+          <GlowCard index={i} key={i} className="p-1">
+            <div
+              key={i}
+              className="aspect-reel relative rounded-xl overflow-clip w-full"
+              ref={video.ref}
+            >
+              <Image
+                src={video.thumbnail}
+                width={1080}
+                height={1920}
+                alt={`${video.title} ${video.description}`}
+                className="w-full h-full object-cover object-center"
+              ></Image>
+              <div className="w-full h-4/5 bg-gradient-to-br from-background-varient via-background-varient/0 absolute top-0 left-0 items-start justify-start">
+                <div className="w-full items-center justify-start flex flex-row gap-1 p-2">
+                  <FontAwesomeIcon
+                    icon={faEye}
+                    className="text-primary ml-2"
+                    size="sm"
+                  ></FontAwesomeIcon>
+                  <p className="text-primary font-monstserrat font-semibold text-xs">
+                    {formatNumber(video.totalViews)}
+                  </p>
+                  <FontAwesomeIcon
+                    icon={faThumbsUp}
+                    className="text-primary ml-2"
+                    size="sm"
+                  ></FontAwesomeIcon>
+                  <p className="text-primary font-monstserrat font-semibold text-xs">
+                    {formatNumber(video.totalLikes)}
+                  </p>
+                </div>
+              </div>
+              <div className="absolute bottom-0 w-full h-1/3 bg-gradient-to-t from-background-varient to-background-varient/0 items-start justify-end flex flex-col px-1 py-2">
+                <h4 className="font-lufga text-primary font-semibold">
+                  {video.title}
+                </h4>
+                <p className="font-montserrat text-primary/50 font-medium text-xs text-justify">
+                  {video.description.length > 100
+                    ? `${video.description.substring(0, 100)}...`
+                    : video.description}
+                </p>
+              </div>
+              <div className="w-1/3 aspect-square rounded-full absolute left-1/2 top-1/2 -translate-1/2 bg-background-varient/50 flex items-center justify-center">
+                <FontAwesomeIcon
+                  icon={faPlay}
+                  size="2xl"
+                  className="text-primary"
+                ></FontAwesomeIcon>
+              </div>
+            </div>
+          </GlowCard>
+        ))}
+      </div>
+    </section>
+  );
+};
+export default VideosFeaturedProjectsSection;
